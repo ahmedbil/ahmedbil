897e6867304faffc7e3912ed8810cf25b57ca7a4
Ahmed Bilal <ahmedbil.157@gmail.com>
2023-05-14 18:51:16 -0400

    updated resume

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
-
-
-
-
-
-
 import "./App.scss";
 
 
